0d3e51bbd0ab7e3d73e3e31d2a0f0d38e17a97ab
Cd <Cd@minint-38uke05.marlabsat.com>
2016-03-30 12:30:50 -0400

    third commit

--- src/Demo.java
+++ src/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("Second time, Hello world!");
+		System.out.println("Third commit.");
 	}
 
 }
